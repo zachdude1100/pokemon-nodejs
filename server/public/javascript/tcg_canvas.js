53945c611666c9ce018a7ac0108df106bd346e4b
zachdude1100 <zachdude1100@gmail.com>
2021-09-19 22:59:17 -0400

    tcg game commented and optimized a tiny bit

--- server/public/javascript/tcg_canvas.js
+++ server/public/javascript/tcg_canvas.js
@@ -2,23 +2,23 @@
 
 var socket;
 socket=io.connect('10.10.0.6:3000')
-socket.on('gameState',updateGameState)
+socket.on('gameState',updateGameState) //upon receiving game state packet from the other player:
 
-var width = document.getElementById('stageContainer').clientWidth;
+var width = document.getElementById('stageContainer').clientWidth; //these probably shouldn't be global (lazy)
 var height = document.getElementById('stageContainer').clientHeight;
     var stage = new Konva.Stage({
         container: 'stageContainer',
         width: width,
         height: height
   });
-var layer = new Konva.Layer();
+var layer = new Konva.Layer(); //not sure if I should be using multiple "layers"
 
 
 
 function updateGameState(gameState){
-    gameState.forEach(card=>{
-      var cardQuery=stage.find('#'+card.id)[0]
-      if (cardQuery!=undefined){
+    gameState.forEach(card=>{ //game state is sent as an array of card objects
+      var cardQuery=stage.find('#'+card.id)[0] //look for the card id on the stage
+      if (cardQuery!=undefined){ //if its there, move it
         tween=new Konva.Tween({
           node:cardQuery,
           duration:0.1,
@@ -26,57 +26,56 @@ function updateGameState(gameState){
           y:card.y
         }).play()
       }
-      else{
+      else{  //if it's not there, draw it for the first time
         let imageObj= new Image();
         imageObj.src=card.src
         imageObj.id=card.id
-        drawCardOnBoard(imageObj,card.x,card.y)
+        drawCardOnCanvas(imageObj,card.x,card.y) //sends the Image(?) object and the position to draw the card
       }
     })
 }
 
-document.addEventListener("click",function(event){
-  var allCardsOnStage=stage.find('.card')
+document.addEventListener("click",function(event){ //global click listener to update game state
+  var allCardsOnStage=stage.find('.card') //finds all on field with the name of card, so all of them
   var stageData=[];
   allCardsOnStage.forEach(card=>{
-    stageData.push({id:card.attrs.id,src:card.attrs.image.src,x:card.attrs.x,y:card.attrs.y})
+    stageData.push({id:card.attrs.id,src:card.attrs.image.src,x:card.attrs.x,y:card.attrs.y}) //all needed info to draw and update a card on the other player's screen
   })
-  socket.emit('gameState',stageData)
+  socket.emit('gameState',stageData) //emits the game state
 })
 
-var deck=[];
+var deck=[];  //misplaced global variables :D
 var cardsInHand=[];
-function initializeDeck(rawDeck){
+function initializeDeck(rawDeck){ //initialize deck called from page script, with the deck selection previous
   rawDeck.forEach(card => {
     var instances=card.instances;
     for (let i=0;i<instances;i++){
-      var rand = Math.random().toString(36).substr(2, 16)
-      deck.push({id: card.id,imageUrl:card.imageUrl, _id:rand})
+      var rand = Math.random().toString(36).substr(2, 16) //generate and add a random unique (hopefully) id
+      deck.push({id: card.id,imageUrl:card.imageUrl, _id:rand}) //smack some objects in that deck
     }
   });
-  shuffle(deck);
+  shuffle(deck); //call shuffle algorithm after init
 }
-function shuffle(shuffleDeck) {
+function shuffle(shuffleDeck) { //stolen from the internet
   var newDeck = [];
   for (var i = 0; i < shuffleDeck.length; i++) {    
       var rand = Math.floor(Math.random() * (i + 1));  
       newDeck[i] = newDeck[rand];
       newDeck[rand] = shuffleDeck[i];
   }
-  deck=newDeck;
-  drawCardFromDeck() //temporary fix for not having a deck to click, auto draws 7
+  deck=newDeck; //write back into deck
+  drawCardFromDeck() //temporarily draws 7 cards automatically
 }  
 
-function drawCardFromDeck(){
+function drawCardFromDeck(){ //will eventually not draw 7, will be one at a time
   for (let i=0; i<7;i++){
     cardsInHand[i]=deck[i]
   }
-  animateHand()
+  animateHand() //call function to "animate" the hand because draw has conflicting wording
 }
 
-function animateHand(){
-  cardCounter=0;
-  document.querySelectorAll('.card').forEach(e => e.remove());
+function animateHand(){ //draw the hand on the screen in the id=hand div
+  document.querySelectorAll('.card').forEach(e => e.remove()); //removes all before redrawing (laziness)
   for (let i = 0; i < cardsInHand.length; i++){
           let cardImage = document.createElement('img');
           cardImage.src = cardsInHand[i].imageUrl;
@@ -87,29 +86,29 @@ function animateHand(){
   
 }
 
-$(document).on('click',".card" ,function(){
-  placeCardInPlay(this.id)
-  removeCardFromHand(this.id);  
+$(document).on('click',".card" ,function(){  //onclick for your hand
+  placeCardInPlay(this.id,this.src)  //grab the id and send it to be placed
+  removeCardFromHand(this.id);  //remove the card from hand global variable
 });
 
 function removeCardFromHand(id){
-  for (let i = 0; i<cardsInHand.length;i++){
+  for (let i = 0; i<cardsInHand.length;i++){ //iterates thru hand
       if (cardsInHand[i]._id === id){
-          cardsInHand.splice(i,1);
-          animateHand();
+          cardsInHand.splice(i,1); //remove it
+          animateHand(); //animate the hand again
           break;
       }
       else{}
   }
 }
 
-function drawCardOnBoard(imageObj,xPosition,yPosition) {
+function drawCardOnCanvas(imageObj,xPosition,yPosition) { //pass it the image object, x and y
   var cardImg = new Konva.Image({
-    name: 'card',
+    name: 'card', //this name is important to be the same for all
     image: imageObj,
     x: xPosition,
     y: yPosition,
-    width: stage.height() /10,
+    width: stage.height() /10,  //arbitrary size scaling
     height: stage.height()/10*1.375,
     draggable: true,
     id: imageObj.id
@@ -123,18 +122,13 @@ function drawCardOnBoard(imageObj,xPosition,yPosition) {
     document.body.style.cursor = 'default';
   });
 
-  layer.add(cardImg);
-  stage.add(layer);
+  layer.add(cardImg); //add card to the layer
+  stage.add(layer); //add layer to the stage
 }
 
-function placeCardInPlay(cardId){
-  for (let i =0;i<cardsInHand.length;i++){
-    if (cardsInHand[i]._id==cardId){
-      let imageObj= new Image();
-      imageObj.src=cardsInHand[i].imageUrl
-      imageObj.id=cardsInHand[i]._id
-      drawCardOnBoard(imageObj,stage.width() / 2 - 200 / 2,stage.height() / 2 - 137 / 2)
-    }
-  }
-
+function placeCardInPlay(cardId,cardSrc){  //loop needed because of the data being stripped from the image  
+  let imageObj= new Image();
+  imageObj.src=cardSrc
+  imageObj.id=cardId
+  drawCardOnCanvas(imageObj,stage.width() / 2 - 200 / 2,stage.height() / 2 - 137 / 2) //arbitrary placement
 }
