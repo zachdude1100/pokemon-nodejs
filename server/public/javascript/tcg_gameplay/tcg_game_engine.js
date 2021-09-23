253c7dc15b8eb1ebadae421ebaaae9b379c0c66b
zachdude1100 <zachdude1100@gmail.com>
2021-09-22 23:00:50 -0400

    removed unneed function

--- server/public/javascript/tcg_gameplay/tcg_game_engine.js
+++ server/public/javascript/tcg_gameplay/tcg_game_engine.js
@@ -6,17 +6,6 @@ var deck= new Deck()
 var hand = new Hand()
 
 
-
-  function removeCardFromHand(id){
-    for (let i = 0; i<hand.cards.length;i++){ //iterates thru hand
-        if (hand.cards[i]._id === id){
-            hand.cards.splice(i,1); //remove it
-            //animateHand(); //animate the hand again
-            break;
-        }
-        else{}
-    }
-  }
   function updateGameState(gameState){
     gameState.forEach(card=>{ //game state is sent as an array of card objects
       var cardQuery=stage.find('#'+card.id)[0] //look for the card id on the stage
